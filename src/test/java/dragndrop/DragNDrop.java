84a746dbf64b5db04190e7a370542dfa37a0fe35
santalovapolina <polinasantalova@yandex.ru>
2022-12-24 18:35:41 +0700

    added assertion

--- src/test/java/dragndrop/DragNDrop.java
+++ src/test/java/dragndrop/DragNDrop.java
@@ -1,7 +1,6 @@
 package dragndrop;
 
 
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 
@@ -15,7 +14,10 @@ public class DragNDrop {
         open("https://the-internet.herokuapp.com");
         $("#content").$$("a").findBy(text("Drag and Drop")).click();
         $("[id=column-a]").dragAndDropTo("[id=column-b]");
-        actions().moveToElement($("[id=column-b]")).clickAndHold().moveByOffset(100,0).release().perform();
+        $("[id=column-a]").$("header").shouldHave(text("B"));
+
+
+        //actions().moveToElement($("[id=column-b]")).clickAndHold().moveByOffset(100,0).release().perform();
 
     }
 }

245f722cf9f0c2c66c6c714f6af7e220a952a54e
santalovapolina <polinasantalova@yandex.ru>
2022-12-31 14:10:10 +0700

    removed extra brackets

--- src/test/java/demoqa/RegistrationFormTest.java
+++ src/test/java/demoqa/RegistrationFormTest.java
@@ -30,7 +30,7 @@ public class RegistrationFormTest {
         $("#userEmail").setValue("test@test.com");
         $("[for=gender-radio-2]").click();
         $("#userNumber").setValue("1234567890");
-        $("#dateOfBirthInput").click(); // click on date of birth input
+        $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("January"); // select from months dropdown
         $(".react-datepicker__year-select").selectOption("1993"); // select from years dropdown
         $(".react-datepicker__month").$$(".react-datepicker__week")
@@ -43,7 +43,7 @@ public class RegistrationFormTest {
         $("#state").click(); // click on states dropdown
         $("#react-select-3-option-0").sibling(0).click(); // set the state
         $("#city").click(); // click on cities dropdown
-        $("#city").lastChild().find(((byText("Lucknow")))).click(); // set the city
+        $("#city").lastChild().find(byText("Lucknow")).click(); // set the city
         $("#submit").pressEnter();
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
         $(".table").shouldHave(text("test@test.com"));

e827c0bc06a0b05ba87167def9aa79c3f7056c98
santalovapolina <polinasantalova@yandex.ru>
2023-01-29 14:21:22 +0700

    added configs, listener, attachments and steps

--- src/test/java/github/SearchTestsWithAllure.java
+++ src/test/java/github/SearchTestsWithAllure.java
@@ -1,8 +1,12 @@
 package github;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -12,10 +16,12 @@ import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
+@Tag("github")
 public class SearchTestsWithAllure {
 
     private static final String REPOSITORY = "eroshenkoam/allure-example";
     public static final int ISSUE_NUMBER = 80;
+
 
     @Test
     void checkIssueWithListener() {
@@ -52,9 +58,12 @@ public class SearchTestsWithAllure {
             $(withText("#" + ISSUE_NUMBER)).should(exist);
         });
 
-
     }
 
+
+    @Severity(SeverityLevel.BLOCKER)
+    @Feature("Issue в репозитории")
+    @Story("Создание Issue")
     @Test
     void checkIssueWithAnnotatedSteps() {
 

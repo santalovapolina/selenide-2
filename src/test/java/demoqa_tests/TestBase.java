78acaea9303b238679a5ee2fab81a112df4b3330
Polina Santalova <110290287+santalovapolina@users.noreply.github.com>
2023-01-30 22:38:19 +0700

    Update TestBase.java

--- src/test/java/demoqa_tests/TestBase.java
+++ src/test/java/demoqa_tests/TestBase.java
@@ -16,10 +16,10 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-//         Configuration.browserSize = "1920x1080";
-//         Configuration.browser = "chrome";
-//         Configuration.browserVersion = "100.0";
-//         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);

51a28d797b4d7b1b6262fcffc2a8f95a385d1cb8
santalovapolina <polinasantalova@yandex.ru>
2023-01-31 20:06:13 +0700

    configs changed

--- src/test/java/demoqa_tests/TestBase.java
+++ src/test/java/demoqa_tests/TestBase.java
@@ -16,10 +16,11 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.remote = System.getProperty("remote");
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
